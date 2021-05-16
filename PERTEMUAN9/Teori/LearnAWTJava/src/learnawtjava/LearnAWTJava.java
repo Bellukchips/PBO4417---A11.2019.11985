7aa55b8d9d9cd469dbb12aa0f443664c840ff44e
lukman akbar <bellukchips@gmail.com>
2021-05-16 11:30:03 +0700

    push pertemuan 9 teori

---
+++ PERTEMUAN9/Teori/LearnAWTJava/src/learnawtjava/LearnAWTJava.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package learnawtjava;
+
+/**
+ *
+ * @author nothing
+ */
+public class LearnAWTJava {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        AWTControl awt = new AWTControl();
+        awt.showEventDemo();
+    }
+    
+}
